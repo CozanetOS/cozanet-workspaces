f924ed149bf55e410b9bb7a023cdb9b00890700c
CozyCrypto <fassdavid722@gmail.com>
2026-07-11 01:56:36 +0100

    feat: scaffold workspace management - src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export * from './types.js';
+export * from './WorkspaceManager/manager.js';
+export * from './LayoutEngine/layout.js';
+
+// Export Workspace classes
+export * from './workspaces/BrowserWorkspace.js';
+export * from './workspaces/CodeWorkspace.js';
+export * from './workspaces/TerminalWorkspace.js';
+export * from './workspaces/NotesWorkspace.js';
+export * from './workspaces/CX7Workspace.js';
+export * from './workspaces/DashboardWorkspace.js';
